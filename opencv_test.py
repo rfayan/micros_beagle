9a7ae29340d0654d4e8068017f021aa6897bd23b
Guilherme Prearo <guiprearo@gmail.com>
2017-11-09 07:50:31 -0200

    Added python-mqtt communication

--- opencv_test.py
+++ opencv_test.py
@@ -3,11 +3,7 @@ from cv2 import *
 cam = VideoCapture(0)   # 0 -> index of camera
 s, img = cam.read()
 if s:    # frame captured without any errors
-    namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
-    imshow("cam-test",img)
-    waitKey(1)
-    destroyWindow("cam-test")
-    imwrite("/home/alarm/simplecv2/foto.jpg",img) #save image
+    imwrite("/home/alarm/micros_beagle/shot.jpg",img) #save image
 
 cam.release()
 destroyAllWindows()
